c05c911aa61cb7e5b55c2a5b03bb0789f98ebbd2
moexx399 <moexx399@umn.edu>
2018-09-24 18:14:22 -0500

    Auto Rework

--- PowerUp CompBot 18.2/Aurdino/LED_Lights_2018_Moe/LED_Lights_2018_Moe.ino
+++ PowerUp CompBot 18.2/Aurdino/LED_Lights_2018_Moe/LED_Lights_2018_Moe.ino
@@ -7,13 +7,13 @@
 #define D_STRIP_2_PIN   3    //Front LED Strip
 #define D_STRIP_3_PIN   7   //Right LED Strip
 
-#define D_PIXEL_CNT_1  58
+#define D_PIXEL_CNT_1  28
 #define D_BUFF_SIZE     4
 int counter = 0;
 // Constructors
-Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(14, D_STRIP_1_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(28, D_STRIP_1_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(D_PIXEL_CNT_1, D_STRIP_2_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(23, D_STRIP_3_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(28, D_STRIP_3_PIN, NEO_GRB + NEO_KHZ800);
 
 // Functions
 
@@ -23,11 +23,11 @@ void setup()
   Serial.println("3026 PowerUP");
   Serial.begin(9600,SERIAL_8N1); 
   Serial1.begin(9600,SERIAL_8N1);
-  pinMode(8, INPUT);
-pinMode(9, INPUT);
-pinMode(10, INPUT);
-pinMode(11, INPUT); //Left Limit Switch
-pinMode(12, INPUT); //Right Limit Switch
+pinMode(11, INPUT);
+pinMode(12, INPUT);
+pinMode(13, INPUT);
+pinMode(NULL, INPUT); //Left Limit Switch
+pinMode(NULL, INPUT); //Right Limit Switch
   initStrip (&strip_1);     //Left LED Strip
   initStrip (&strip_2);     //Front LED Strip
   initStrip (&strip_3);     //Right LED Strip
@@ -36,116 +36,116 @@ pinMode(12, INPUT); //Right Limit Switch
 // Main Loop
 void loop() {
 int sum = 0;
-if(digitalRead(8))
+if(digitalRead(11))
    sum = sum + 1;
-if(digitalRead(9))
+if(digitalRead(12))
    sum = sum + 2;
-if(digitalRead(10))
+if(digitalRead(13))
    sum = sum + 4;
    
   switch (sum) {  //LED DRIVE MODE
-    case (0):
+    case (1):
       Serial.println("CLIMB");
       setColor(&strip_3, E_WHITE);
       setColor(&strip_2, E_WHITE);
       setColor(&strip_1, E_WHITE); 
       break;
       
-    case (1):
+    case (0):
      Serial.println("Robot Centric");
      
-     if (digitalRead(11)||digitalRead(12)) { 
-        if(digitalRead(11)&&(!digitalRead(12))) { 
-        Serial.println("11");
-        blinkColor(&strip_1, E_BLUE, 250);
-        setColor(&strip_2, E_BLUE);
-        setColor(&strip_3, E_BLUE);}
-        else if(digitalRead(12)&&(!digitalRead(11))){
-        Serial.println("12");
-        setColor(&strip_1, E_BLUE);
-        setColor(&strip_2, E_BLUE);
-        blinkColor(&strip_3, E_BLUE, 250);}
-        else {
-          Serial.println("Both");
-        blinkColor(&strip_1, E_BLUE, 250);
-        setColor(&strip_2, E_BLUE);
-        blinkColor(&strip_3, E_BLUE, 250);}
-        }
-     else {
+//     if (digitalRead()||digitalRead(12)) { 
+//        if(digitalRead(11)&&(!digitalRead(12))) { 
+//        Serial.println("11");
+//        blinkColor(&strip_1, E_BLUE, 250);
+//        setColor(&strip_2, E_BLUE);
+//        setColor(&strip_3, E_BLUE);}
+//        else if(digitalRead(12)&&(!digitalRead(11))){
+//        Serial.println("12");
+//        setColor(&strip_1, E_BLUE);
+//        setColor(&strip_2, E_BLUE);
+//        blinkColor(&strip_3, E_BLUE, 250);}
+//        else {
+//          Serial.println("Both");
+//        blinkColor(&strip_1, E_BLUE, 250);
+//        setColor(&strip_2, E_BLUE);
+//        blinkColor(&strip_3, E_BLUE, 250);}
+//        }
+//     else {
       setColor(&strip_1, E_BLUE);
       setColor(&strip_3, E_BLUE);
       setColor(&strip_2, E_BLUE);
-      }
+//      }
       
      break;
         
     case (2):
       Serial.println("Field Centric");
       
-     if (digitalRead(11)||digitalRead(12)) { 
-        if(digitalRead(11)&&(!digitalRead(12))) { 
-        blinkColor(&strip_1, E_GREEN, 250);
-        setColor(&strip_2, E_GREEN);
-        setColor(&strip_3, E_GREEN);}
-        else if(digitalRead(12)&&(!digitalRead(11))){
-        setColor(&strip_1, E_GREEN);
-        setColor(&strip_2, E_GREEN);
-        blinkColor(&strip_3, E_GREEN, 250);}
-        else {
-        blinkColor(&strip_1, E_GREEN, 250);
-        setColor(&strip_2, E_GREEN);
-        blinkColor(&strip_3, E_GREEN, 250);}
-        }
-     else {
+//     if (digitalRead(11)||digitalRead(12)) { 
+//        if(digitalRead(11)&&(!digitalRead(12))) { 
+//        blinkColor(&strip_1, E_GREEN, 250);
+//        setColor(&strip_2, E_GREEN);
+//        setColor(&strip_3, E_GREEN);}
+//        else if(digitalRead(12)&&(!digitalRead(11))){
+//        setColor(&strip_1, E_GREEN);
+//        setColor(&strip_2, E_GREEN);
+//        blinkColor(&strip_3, E_GREEN, 250);}
+//        else {
+//        blinkColor(&strip_1, E_GREEN, 250);
+//        setColor(&strip_2, E_GREEN);
+//        blinkColor(&strip_3, E_GREEN, 250);}
+//        }
+//     else {
       setColor(&strip_1, E_GREEN);
       setColor(&strip_3, E_GREEN);
       setColor(&strip_2, E_GREEN);
-      }     
+//      }     
       break;
           
     case (3):
       Serial.println("Tracks Down");
       trackingEye(&strip_2, E_RED, 50, counter);
 
-       if (digitalRead(11)||digitalRead(12)) { 
-        if(digitalRead(11)&&(!digitalRead(12))) { 
-        blinkColor(&strip_1, E_RED, 250);
-        setColor(&strip_3, E_RED);}
-        else if(digitalRead(12)&&(!digitalRead(11))){
-        setColor(&strip_1, E_RED);
-        blinkColor(&strip_3, E_RED, 250);}
-        else {
-        blinkColor(&strip_1, E_RED, 250);
-        blinkColor(&strip_3, E_RED, 250);}
-        }
-     else {
+//       if (digitalRead(11)||digitalRead(12)) { 
+//        if(digitalRead(11)&&(!digitalRead(12))) { 
+//        blinkColor(&strip_1, E_RED, 250);
+//        setColor(&strip_3, E_RED);}
+//        else if(digitalRead(12)&&(!digitalRead(11))){
+//        setColor(&strip_1, E_RED);
+//        blinkColor(&strip_3, E_RED, 250);}
+//        else {
+//        blinkColor(&strip_1, E_RED, 250);
+//        blinkColor(&strip_3, E_RED, 250);}
+//        }
+//     else {
       setColor(&strip_1, E_RED);
       setColor(&strip_3, E_RED);
-      } 
+//      } 
       break;
 
    case (4):
       Serial.println("NULL");
       
-     if (digitalRead(11)||digitalRead(12)) { 
-        if(digitalRead(11)&&(!digitalRead(12))) { 
-        blinkColor(&strip_1, E_ORANGE, 250);
-        setColor(&strip_2, E_ORANGE);
-        setColor(&strip_3, E_ORANGE);}
-        else if(digitalRead(12)&&(!digitalRead(11))){
-        setColor(&strip_1, E_ORANGE);
-        setColor(&strip_2, E_ORANGE);
-        blinkColor(&strip_3, E_ORANGE, 250);}
-        else {
-        blinkColor(&strip_1, E_ORANGE, 250);
-        setColor(&strip_2, E_ORANGE);
-        blinkColor(&strip_3, E_ORANGE, 250);}
-        }
-     else {
+//     if (digitalRead(11)||digitalRead(12)) { 
+//        if(digitalRead(11)&&(!digitalRead(12))) { 
+//        blinkColor(&strip_1, E_ORANGE, 250);
+//        setColor(&strip_2, E_ORANGE);
+//        setColor(&strip_3, E_ORANGE);}
+//        else if(digitalRead(12)&&(!digitalRead(11))){
+//        setColor(&strip_1, E_ORANGE);
+//        setColor(&strip_2, E_ORANGE);
+//        blinkColor(&strip_3, E_ORANGE, 250);}
+//        else {
+//        blinkColor(&strip_1, E_ORANGE, 250);
+//        setColor(&strip_2, E_ORANGE);
+//        blinkColor(&strip_3, E_ORANGE, 250);}
+//        }
+//     else {
       setColor(&strip_1, E_ORANGE);
       setColor(&strip_2, E_ORANGE);
       setColor(&strip_3, E_ORANGE);
-      }
+//      }
       break;
 
    case (5):
@@ -180,10 +180,10 @@ if(digitalRead(10))
       
    case(7):
       Serial.println("START_UP");
-      if(digitalRead(4))
+//      if(digitalRead(4))
       RunningLights(&strip_1, &strip_2, &strip_3, 58 , 255, 0, 0, 50);
-      else
-      RunningLights(&strip_1, &strip_2, &strip_3, 58 , 0, 0, 255, 50);
+//      else
+//      RunningLights(&strip_1, &strip_2, &strip_3, 58 , 0, 0, 255, 50);
       break;
 
    default:
@@ -192,8 +192,3 @@ if(digitalRead(10))
 
         Serial.println(digitalRead(4));
   } } // End switch
-
-
-
-
-
---
+++ PowerUp CompBot 18.2/Paths/Path Segments/READ_ME.txt
@@ -0,0 +1,8 @@
+None of these paths are flipped. To drive backward flip the left and right side. 
+Then negate the position and velocity.
+
+Right Scale 24, 6.5, 0
+
+Left Scale 24, 20.5, 0
+
+5 blank lines between segments
